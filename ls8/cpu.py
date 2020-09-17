4a245f40d46ed2f50095b8191fe7553096bcf62c
stevedole808 <stevedolelambda@gmail.com>
2020-09-16 23:31:17 -1000

    Finished Day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,16 @@ class CPU:
         self.ram = [0] * 256 #  256 bites memory
         self.pc = 0
         self.running = True
+        self.codes = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100010: self.MUL,
+            0b10100011: self.DIV,
+            0b10100000: self.ADD,
+            0b10100001: self.SUB
+        }
+    
     
     def ram_read(self, MAR):
         # Accepts the address to read and returns the value stored there
@@ -23,31 +33,61 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
 
         # For now, we've just hardcoded a pr ogram:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
+        if len(sys.argv) != 2:
+            print("usage: cpu.py filename")
+            sys.exit(1)
+        try:
+            address = 0
+
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    t = line.split('#')
+                    n = t[0].strip()
+
+                    if n == '':
+                        continue
+                    try:
+                        n = int(n, 2)
+                    except ValueError:
+                        print(f"Invalid number '{n}'")
+                        sys.exit(1)
+                        
+                    self.ram[address] = n
+                    address += 1
+        
+        except FileNotFoundError:
+            print(f"File not Found: {sys.argv[1]}")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "SUB": 
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] //= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,39 +110,57 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    
+    def LDI(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.reg[operand_a] = operand_b
 
+    def PRN(self):
+        operand_a = self.ram_read(self.pc + 1)
+        print(self.reg[operand_a])
+
+    def HLT(self):
+        exit()
+
+    def MUL(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("MUL", operand_a, operand_b)
+    
+    def DIV(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("DIV", operand_a, operand_b)
+
+    def ADD(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("ADD", operand_a, operand_b)
+
+    def SUB(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu("SUB", operand_a, operand_b)
+        
     def run(self):
         """Run the CPU."""
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
 
+        self.running = True
+        
         while self.running:
             ir = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-
-            if ir == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-
-            elif ir == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-
-            elif ir == HLT:
-                self.running = False
-                
-            else:
-                self.trace()
-                self.running = False
+            self.codes[ir]()
+            number_of_operands = (ir & 0b11000000) >> 6
+            how_far_to_move_pc = number_of_operands + 1
+            self.pc += how_far_to_move_pc 
 
         # if ir == '00000001':
         #     running = false 
         #     pc += 1
         # elif ir == '10000010':
         #     self.reg[operand_a] = 
-
+ 
     # def HLT():
     #     exit()
 
